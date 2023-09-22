b5eb7d1e3472537de9fb4d712cf8070fbf87d624
Alvarosanti <alvarosantisteban56@gmail.com>
2023-09-22 09:40:25 -0500

    up env  variables

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -63,10 +63,10 @@ const Contact = () => {
     emailjs
       .send(
         // import.meta.env.VITE_SERVICE_ID,
-        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.serviceid,
 
         // import.meta.env.VITE_TEMPLATE_ID,
-        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        process.env.templateid,
         {
           from_name: form.name,
           to_name: "DALSEB ENERGY",
@@ -75,7 +75,7 @@ const Contact = () => {
           message: form.message,
         },
         // import.meta.env.VITE_PUBLIC_KEY
-        process.env.NEXT_PUBLIC_PUBLIC_KEY
+        process.env.publickey
       )
       .then(
         () => {
